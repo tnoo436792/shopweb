4b3f3014abe7b857245f5cc46f4556eb147d2196
tnoo436792 <30401693+tnoo436792@users.noreply.github.com>
2018-12-06 21:42:56 +0800

    Add files via upload

---
+++ web99/js/banner.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'horizontal',
+	  
+    captions: true,
+	  
+	  auto:true,
+	  
+	  speed:1500,
+	  
+	  pause:3000,
+	  
+	  
+    
+  });
+});
